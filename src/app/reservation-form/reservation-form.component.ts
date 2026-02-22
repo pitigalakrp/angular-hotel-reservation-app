45d4c3a13fe7051428c154025c8a326e01c65856
Ravindu Panthaka <pkravindu123@gmail.com>
2026-02-22 14:44:05 +0530

    Added http requests to the CRUD operations

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -33,10 +33,11 @@ export class ReservationFormComponent implements OnInit {
     let id = this.activatedRoute.snapshot.paramMap.get('id')
 
     if(id) {
-      let reservation = this.reservationService.getReservation(id)
-      if (reservation){
-        this.reservationForm.patchValue(reservation)
-      }
+      this.reservationService.getReservation(id).subscribe(reservation => {
+        if (reservation){
+          this.reservationForm.patchValue(reservation)
+        }
+      })
     }
   }
 
@@ -47,9 +48,9 @@ export class ReservationFormComponent implements OnInit {
       let id = this.activatedRoute.snapshot.paramMap.get('id')
 
       if(id) {
-        this.reservationService.updateReservation(id, reservation)
+        this.reservationService.updateReservation(id, reservation).subscribe(() => console.log('Update Request Processed'))
       } else{
-        this.reservationService.addReservation(reservation)
+        this.reservationService.addReservation(reservation).subscribe(() => console.log('Added Request Processed'))
       }
 
 
--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -14,11 +14,13 @@ export class ReservationListComponent {
   constructor(private reservationService: ReservationService){}
 
   ngOnInit(): void{
-    this.reservations = this.reservationService.getReservations();
+    this.reservationService.getReservations().subscribe(reservations => {
+      this.reservations = reservations
+    });
   }
 
   deleteReservation(id: string){
-    this.reservationService.deleteReservation(id)
+    this.reservationService.deleteReservation(id).subscribe(() => console.log('Delete Requeste Processed'))
   }
 
 }
